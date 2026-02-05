f3ed83dc3193858fcb1d0e22b1f3871e6a3214e0
Askar Nasyrov <ACKAP.mail@bk.ru>
2026-02-05 16:21:54 +0300

    Initial commit

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,26 @@
+from advertisements.serializers import AdvertisementSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework.viewsets import ModelViewSet
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.models import Advertisement
+from advertisements.filters import AdvertisementFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create"]:
             return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsOwnerOrReadOnly()]
+
         return []
